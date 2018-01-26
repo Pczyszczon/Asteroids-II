29030ae353d3b079fa502b270223b2594ce58814
Pczyszczon <pawel.czyszczon997@gmail.com>
2018-01-26 21:11:25 +0100

    Add files via upload

---
+++ Assets/Rocket Pack/RocketController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+
+	private float speed = 7;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+	}
+		
+}
